2702e42f7aa6b97f6c1594b02016b89c41543780
S0rrow <jae9hell@gmail.com>
2022-01-13 17:14:36 +0900

    App04 bug inducing commit

--- app/src/main/java/gradleTest/Increment/App04.java
+++ app/src/main/java/gradleTest/Increment/App04.java
@@ -1,27 +1,13 @@
 package gradleTest.Increment;
 
 public class App04 {
-    public static int getSum(int n)
+    public static double getAngle(int x1, int y1, int x2, int y2)
     {
-        int total = 0;
-
-        if(n > 0)
-        {
-            while(n > 0)
-            {
-                total += n;
-                n -= 1;
-            }
-        }
-        else
-        {
-            while(n < 0)
-            {
-                total += n;
-                n += 1;
-            }
-        }
-
-        return total;
+        int dx = x2 - x1;
+        int dy = y2 - y1;
+        double d = dx/dy;
+        //double angle = Math.atan2(dy, dx);
+        double angle = Math.atan(d);
+        return angle;
     }
 }
