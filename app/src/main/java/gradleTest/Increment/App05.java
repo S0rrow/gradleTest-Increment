51b31562b78fc02fb3ef065d6dd81b7ec68a131a
S0rrow <jae9hell@gmail.com>
2022-01-13 17:15:44 +0900

    App05 bug inducing commit

--- app/src/main/java/gradleTest/Increment/App05.java
+++ app/src/main/java/gradleTest/Increment/App05.java
@@ -1,5 +1,31 @@
 package gradleTest.Increment;
 
 public class App05 {
-    
+    public static int getSum(int n)
+    {
+        int total = 0;
+
+        if(n < 0)
+        {
+            while(n > 0)
+            {
+                total += n;
+                n -= 1;
+            }
+        }
+        else
+        {
+            while(n < 0)
+            {
+                total += n;
+                n += 1;
+            }
+        }
+
+        return total;
+    }
+
+    public static int getSumFromTo(int from, int to){
+        return getSum(to) - getSum(from); // +from;
+    }
 }
