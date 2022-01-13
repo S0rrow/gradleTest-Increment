ec5a1426d5743284dc632a25f08e5624a77a6c4c
S0rrow <jae9hell@gmail.com>
2022-01-13 16:57:02 +0900

    10 java classes added

---
+++ app/src/main/java/gradleTest/Increment/App06.java
@@ -0,0 +1,27 @@
+package gradleTest.Increment;
+
+public class App06 {
+    public static int getSum(int n)
+    {
+        int total = 0;
+
+        if(n > 0)
+        {
+            while(n > 0)
+            {
+                total += n;
+                n -= 1;
+            }
+        }
+        else
+        {
+            while(n < 0)
+            {
+                total += n;
+                n += 1;
+            }
+        }
+
+        return total;
+    }
+}
