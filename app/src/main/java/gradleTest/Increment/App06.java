4e0019ae6268b6535b8f41b3e4e77738eb8c1c46
S0rrow <jae9hell@gmail.com>
2022-01-13 17:19:51 +0900

    App06 bug inducing commit

--- app/src/main/java/gradleTest/Increment/App06.java
+++ app/src/main/java/gradleTest/Increment/App06.java
@@ -1,27 +1,10 @@
 package gradleTest.Increment;
 
 public class App06 {
-    public static int getSum(int n)
-    {
-        int total = 0;
-
-        if(n > 0)
-        {
-            while(n > 0)
-            {
-                total += n;
-                n -= 1;
-            }
-        }
-        else
-        {
-            while(n < 0)
-            {
-                total += n;
-                n += 1;
-            }
-        }
-
-        return total;
+    public static double getDist(int x1, int y1, int x2, int y2) {
+        int dx = x2 - x1;
+        int dy = y2 - y1;
+        double sqDist = dx * dx + dy * dy;
+        return sqDist;//Math.sqrt(sqDist);
     }
 }
